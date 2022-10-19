64312483bce19b9741f5d5ab34c3d24efa8315c5
mploythai <110638329+mploythai@users.noreply.github.com>
2022-10-18 23:34:47 -0700

    implemented yahya and juan's work

--- app.py
+++ app.py
@@ -1,77 +1,44 @@
 from quart import Quart, request, make_response, render_template_string
 from functools import wraps
 import sqlite3
-import uuid
 import base64
 import hashlib
 import secrets
+import uuid
 import json
 
 app = Quart(__name__)
-
-# connect to sqlite db
 connect = sqlite3.connect("data.db")
 cursor = connect.cursor()
 
-# for password encryption
 ALGORITHM = "pbkdf2_sha256"
 
-# authentication
+
 def auth(fcn):
     @wraps(fcn)
     async def wrapper(*args, **kwargs):
+        cursor.execute(
+            "create table if not exists accounts (id text, username text not null, password text not null)"
+        )
+
         auth = request.authorization
-        # accounts table
-        cursor.execute(
-            "create table if not exists accounts (id text primary key, username text not null, password text not null)"
-        )
-
-        # make the correct and valid words table
-        cursor.execute(
-            "create table if not exists correctWords (word text)"
-        )
-        cursor.execute(
-            "create table if not exists validWords (word text)"
-        )
-
-        #prevents repopulating if the tables do not need to be created
-        if cursor.fetchone() != None:
-            print("populating")
-            #insert the words from the files into their tables
-            jsonFile = open("correct.json")
-            correctJson = json.load(jsonFile)
-            for i in correctJson:
-                cursor.execute(
-                    f"insert into correctWords values ('{i}')"
-                )
-
-            jsonFile = open("valid.json")
-            validJson = json.load(jsonFile)
-            for i in validJson:
-                cursor.execute(
-                    f"insert into validWords values ('{i}')"
-                )
-
-
         if auth and auth.type == "basic" and auth.username and auth.password:
-            # check if user exists in db based in user input
+            user = str(auth.username)
+            reformattedUser = user.replace(" ", "_")
+
             data = cursor.execute(
-                f"select * from accounts where username='{auth.username}'"
+                f"select * from accounts where username='{reformattedUser}'"
             ).fetchone()
 
-            # if the data does exist, check if their password input
-            # is correct, then authenticate the user if it is
-            # else, put their info into the db and then authenticate
             if data:
-                if verify_password(f"{auth.password}", f"{data[2]}"):
+                if verifyPass(f"{auth.password}", f"{data[2]}"):
                     return await fcn(*args, **kwargs)
             else:
-                register(auth.username, auth.password)
+                register(reformattedUser, auth.password)
                 return await fcn(*args, **kwargs)
 
-        # if user presses cancel, return 401 response
         return await make_response(
-            "Could not verify!",
+            "Couldn not verify!",
             401,
             {"WWW-Authenticate": 'Basic realm="Login required"'},
         )
@@ -79,66 +46,104 @@ def auth(fcn):
     return wrapper
 
 
-# create new user if user dne in the db
+def verifyPass(pwd, pwdHash):
+    if (pwdHash or "").count("$") != 3:
+        return False
+    algo, iter, salt, hash = pwdHash.split("$", 3)
+    iter = int(iter, base=10)
+    assert algo == ALGORITHM
+    compare = hashPass(pwd, salt, iter)
+    return secrets.compare_digest(pwdHash, compare)
+
+
+def hashPass(pwd, salt=None, iter=260000):
+    if salt is None:
+        salt = secrets.token_hex(16)
+    assert salt and isinstance(salt, str) and "$" not in salt
+    assert isinstance(pwd, str)
+    pwHash = hashlib.pbkdf2_hmac(
+        "sha256", pwd.encode("utf-8"), salt.encode("utf-8"), iter
+    )
+    hash = base64.b64encode(pwHash).decode("ascii").strip()
+    return "{}${}${}${}".format(ALGORITHM, iter, salt, hash)
+
+
 def register(user, pwd):
-    encrypted_pwd = hash_password(pwd)
+    encryptedPwd = hashPass(pwd)
     cursor.execute(
-        f"insert into accounts values ('{uuid.uuid4()}', '{user}', '{encrypted_pwd}')"
+        f"insert into accounts values ('{uuid.uuid4()}', '{user}', '{encryptedPwd}')"
     )
     connect.commit()
 
 
-# password encryption
-def hash_password(password, salt=None, iterations=260000):
-    if salt is None:
-        salt = secrets.token_hex(16)
-    assert salt and isinstance(salt, str) and "$" not in salt
-    assert isinstance(password, str)
-    pw_hash = hashlib.pbkdf2_hmac(
-        "sha256", password.encode("utf-8"), salt.encode("utf-8"), iterations
-    )
-    b64_hash = base64.b64encode(pw_hash).decode("ascii").strip()
-    return "{}${}${}${}".format(ALGORITHM, iterations, salt, b64_hash)
-
-
-# password verification
-def verify_password(password, password_hash):
-    if (password_hash or "").count("$") != 3:
-        return False
-    algorithm, iterations, salt, b64_hash = password_hash.split("$", 3)
-    iterations = int(iterations, base=10)
-    assert algorithm == ALGORITHM
-    compare_hash = hash_password(password, salt, iterations)
-    return secrets.compare_digest(password_hash, compare_hash)
-
-
-# main page
 @app.route("/")
 @auth
 async def index():
     return {"authenticated": True}, 200
 
 
-# page to check if data is being pushed to db properly
 @app.route("/data")
 @auth
 async def data():
     data = cursor.execute("select * from accounts").fetchall()
+
     return await render_template_string(f"{data}")
 
 
-# game page
-@app.route("/startNewGame")
+@app.route("/game")
 @auth
 async def game():
-    #select random word
+    # code by yahya
+    createWordTables()
     data = cursor.execute(
         "select word from correctWords order by Random() limit 1"
     ).fetchone()
-    
-    #insert the new game into the user table
-    print(data[0])
-    return data[0]
+
+    return await createGameTable(data[0])
+
+
+def createWordTables():
+    # code by yahya
+    correct = cursor.execute("create table if not exists correctWords (word text)")
+    valid = cursor.execute("create table if not exists validWords (word text)")
+
+    if not correct.fetchone():
+        jsonFile = open("data/correct.json")
+        file = json.load(jsonFile)
+        for i in file:
+            cursor.execute(f"insert into correctWords values ('{i}')")
+
+    if not valid.fetchone():
+        jsonFile = open("data/valid.json")
+        file = json.load(jsonFile)
+        for i in file:
+            cursor.execute(f"insert into validWords values ('{i}')")
+
+
+async def createGameTable(word):
+    # code by juan
+    user = cursor.execute(
+        f"select username from accounts where username='mike-ploythai'"
+    ).fetchone()
+    tableName = f"{word}_{user}"
+
+    cursor.execute(
+        f"create table if not exists {tableName} (id text, word text, tries numeric, history text)"
+    )
+    cursor.execute(
+        f"insert into {tableName} values ('{uuid.uuid4()}', '{word}', 5, '')"
+    )
+    connect.commit()
+
+    return await render_template_string(
+        f"{cursor.execute(f'select * from {tableName}').fetchall()}"
+    )
+
+
+@app.route("/prev-game")
+@auth
+async def prevGame():
+    return
 
 
 if __name__ == "__main__":
